fd83e9329b922bc4279f5ac20345beef67fcc1cb
rohidtzz <63803545+rohidtzz@users.noreply.github.com>
2024-06-11 00:47:27 +0700

    fixing add models to seedr

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 use App\Models\User;
+use App\Models\Product;
+use App\Models\ProductCategory;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
